ac51d485d2e1966b338465496f06bb28f480bfdc
julia.buerk <julia.buerk@dhbw-engineering.de>
2024-04-14 00:41:30 +0200

    add lane clustering

--- src/lane_detection.py
+++ src/lane_detection.py
@@ -15,28 +15,6 @@ class LaneDetection:
     def __init__(self):
         self.evaluate = False #ConfigLaneDetection.evaluate
         
-    def detect(self, image: np.ndarray):
-        print(image.shape)
-        
-        image = image[:,:,:3]
-        
-        # crop info display
-        image = image[:image.shape[0]-ConfigLaneDetection.displaycrop, :, :]
-        
-        # mask the car
-        image[CarConst.start_h:CarConst.end_h, 
-              CarConst.start_w:CarConst.end_w, 
-              :] = np.zeros((102, 68, 3))
-        
-        image = edge_detect_scipy(image) if not self.evaluate else self.evaluation_detect(image)
-        
-        show_plt_img_grey(image=image)
-        
-        # segment left/right lane 
-        # image = self.left_right_lane_detection(image)
-        
-        return image
-        
     def evaluation_detect(self, image):
         images = {
             "green": detect_green_pixels(image, threshold=50),
@@ -54,6 +32,95 @@ class LaneDetection:
         plt.show()
         
         return images["scipy"]
+        
+    def detect(self, image: np.ndarray):
+        print(image.shape)
+        
+        image = image[:,:,:3]
+        
+        # crop info display
+        image = image[:image.shape[0]-ConfigLaneDetection.displaycrop, :, :]
+        
+        # mask the car
+        image[CarConst.start_h:CarConst.end_h, 
+              CarConst.start_w:CarConst.end_w, 
+              :] = np.zeros((102, 68, 3))
+        
+        image = edge_detect_scipy(image) if not self.evaluate else self.evaluation_detect(image)
+        
+        show_plt_img_grey(image=image)
+        
+        image = (image > 40).astype(np.uint8)
+        
+        # segment left/right lane 
+        image = self.lane_clustering(image, 50)
+        
+        plt.imshow(image, cmap='viridis')  # Choose a suitable colormap, such as 'viridis'
+        plt.colorbar()  # Add colorbar for reference
+        plt.title('Lane Clusters')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+        
+        return image
+
+    def lane_clustering(self, image: np.ndarray, min_points: int) -> np.ndarray:
+        """
+        - map all values to a 2d numpy array with either ones or zeros.
+        - minimal threshold and start with a random 1
+        - Detect all the neighbour 1s and append to cluster. 
+        - Safe the cluster if enough points are in the cluster.
+
+        Clusters are enumerated, so all pixels with the same value are from one cluster.
+        Cluster enumeration start with 2 
+
+        in the end, the returning array consists of the background (0) all clusters starting at 2 
+
+        Uses: 
+            find_neighbors -> returns a list of all coordinates of a cluster.
+
+        Args:
+            image (np.ndarray): grey rgb image of shape (h, w, 3)
+
+        Returns:
+            np.ndarray: array with the clusters.
+        """
+        h, w = image.shape
+        current_cluster = 2 
+
+        for y in range(h):
+            for x in range(w):
+                if image[y, x] == 1:  
+                    cluster_coords = find_neighbors(image, x, y, set()) 
+                    if len(cluster_coords) > min_points:
+                        for coord in cluster_coords:
+                            image[coord[1], coord[0]] = current_cluster 
+                        current_cluster += 1  
+        return image
+        
+def find_neighbors(image: np.ndarray, x: int, y: int, neighbors:set) -> set[tuple[int, int]]:
+    """
+    recursive function that counts the neighbor 1s and appends the coordinates to an array.
+
+    Args:
+        image (np.ndarray): _description_
+        x (int): _description_
+        y (int): _description_
+
+    Returns:
+        Tuple[np.ndarray, List[Tuple[int, int]]]: Updated image and list of neighbor coordinates.
+    """
+    h, w = image.shape
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < w and 0 <= ny < h and image[ny, nx] == 1:
+            image[ny][nx] = 0
+            neighbors.add((nx, ny))
+            new_neighbors = find_neighbors(image, nx, ny, neighbors)  
+            neighbors.add(new_neighbors)
+    return neighbors
 
 
 if __name__ == "__main__":
